4a45af09781ed9becce3f77880e6868d4c3d3d50
wmiq84 <michaelwang3489@gmail.com>
2024-09-06 15:45:31 -0700

    Add modal form

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+const newBook = document.querySelector('.new-book');
 const list = document.querySelector('.book-list');
+const popUp = document.querySelector('dialog');
+const submit = document.querySelector('.submit')
 
 const myLibrary = ['The Stand', 'Chainsaw Man'];
 
@@ -8,18 +11,33 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
   this.info = function() {
-      return title  + ' by ' + author + ', ' + pages + ", " + read;
+      return title  + ' by ' + author + ', ' + pages + ', ' + read;
   }
 }
 
 
 function addBookToLibrary(array) {
   for (const book of array) {
-    const listItem = document.createElement("ul");
+    const listItem = document.createElement('ul');
     listItem.textContent = book;
     list.appendChild(listItem);
   }
 }
+
+newBook.addEventListener('click', () => {
+  console.log("New book added.");
+  popUp.showModal();
+});
+
+submit.addEventListener('click', updateLibrary); 
+
+function updateLibrary(event) {
+  console.log("Form submitted.");
+  event.preventDefault();
+}
+
+
+
 
 addBookToLibrary(myLibrary);
 
